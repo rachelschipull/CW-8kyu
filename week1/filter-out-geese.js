bf9f52b967dee01f9e0fd360525aa2040c9a8880
rachelschipull <rschipull@gmail.com>
2022-04-04 18:27:17 -0400

    add filter-out-geese to 8kyu

---
+++ week1/filter-out-geese.js
@@ -0,0 +1,8 @@
+//Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    
+    // return an array containing all of the strings in the input array except those that match strings in geese
+  return birds.filter(x => !geese.includes(x))
+  };
