6c0e03698d2775b4671d8ac90f82cc6f5e601984
rachelschipull <rlschipull@gmail.com>
2022-04-09 13:39:05 -0400

    add

---
+++ week2/num-then-sum.js
@@ -0,0 +1,11 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+
+function sumMix(x){
+    //map or math to turn all strings to integers
+     const allNum = x.map(num => Number(num));
+     // use reduce to sum the numbers together
+    return allNum.reduce((a, b) => a + b);
+   }
