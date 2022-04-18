042ed25e43e41cad94ede6fab83d414467aa6b0b
rachelschipull <rlschipull@gmail.com>
2022-04-18 18:19:33 -0400

    add new files

---
+++ week3/multiply-basic.js
@@ -0,0 +1,7 @@
+//multiplying two numbers
+
+//Implement a function which multiplies two numbers.
+
+function multiply( a, b ){
+    return a * b;
+  }
---
+++ week3/under-pressure-1.js
@@ -0,0 +1,8 @@
+//code under pressure #1
+
+//Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+  
+    return i*2;
+  }
