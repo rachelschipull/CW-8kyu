ea7e828ea51502b89c13cacec485a663f16dc529
rachelschipull <rlschipull@gmail.com>
2022-05-24 18:52:05 -0400

    add codewars 5-24-22

---
+++ week8/merge-two-sorted-arrays-into-one.js
@@ -0,0 +1,27 @@
+// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+// Examples (input -> output)
+// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+// Happy coding!
+
+function mergeArrays(arr1, arr2) {
+    //P: two arrays of sorted integers, could be empty, 0, or a bunch of integers, may be repeats
+    //R: one sorted array (in asc order) with no repeated integers
+    //E: [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // [], [1, 2, 3, 4, 5] -> [1, 2, 3, 4, 5]
+    //P: create a new array
+    // add the two arrays to the new array
+    // sort and remove duplicates
+    //return new array
+    const arr3 = [... new Set(arr1.concat(arr2))]
+    return arr3.sort((a,b) => a-b, 0)
+}
