65c46d9d568b134638939a5026874d49fd71ecb4
rachelschipull <rlschipull@gmail.com>
2022-06-26 23:28:15 -0400

    add codewars

---
+++ week13/write-function-get-char.js
@@ -0,0 +1,8 @@
+//Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+//find the correct function for this problem, as otherwise it would require writing a while library locally
+
+function getChar(c) {
+    return String.fromCharCode(c)
+}
+
