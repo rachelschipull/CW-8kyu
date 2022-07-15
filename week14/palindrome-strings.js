34b5f7d4774f969038bc348aef061df3c076134b
rachelschipull <rlschipull@gmail.com>
2022-07-15 18:55:24 -0400

    update codewars

--- week14/palindrome-strings.js
+++ week14/palindrome-strings.js
@@ -1,4 +1,4 @@
-
+//Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
 
 function isPalindrome(line) {
     let lineString = line.toString()
@@ -8,3 +8,9 @@ function isPalindrome(line) {
         return false
     }
 }
+
+//using implicit return (which is how I wanted to do it)
+
+// function isPalindrome(line) {
+//     return (line.toString() == line.toString().split('').reverse().join(''));
+//   }
