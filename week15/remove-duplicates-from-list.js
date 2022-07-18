1a08728c575b479ca3071a3c7e4427a771719180
rachelschipull <rlschipull@gmail.com>
2022-07-18 16:04:41 -0400

    add another codewars

---
+++ week15/remove-duplicates-from-list.js
@@ -0,0 +1,21 @@
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+
+//this is the simplest way of doing it
+function distinct(a) {
+    return [...new Set(a)];
+}
+
+//this is the way I thought about doing it
+// function distinct(arr) {
+//     let res = []; 
+    
+//     for(let i = 0; i < arr.length; i++){
+//         if(!res.includes(arr[i])){
+//         res.push(arr[i]);
+//     }
+// }
+// return res;
+// }
